a8253a1ca480abf8b2bb7a2489963ecbbe732fb5
Yauheni Rudziankou <yrudziankou@itsupportme.com>
2021-05-31 16:10:49 +0300

    feat: added link to posts

--- src/components/router-components/home-page/HomePage.jsx
+++ src/components/router-components/home-page/HomePage.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './HomePage.css'
+import { Link } from 'react-router-dom'
 
-const Home = () => <div>Home</div>
+const Home = () => (
+  <div>
+    <Link to="/posts">Posts</Link>
+  </div>
+)
 
 export default Home
